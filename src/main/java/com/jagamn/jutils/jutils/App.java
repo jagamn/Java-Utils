05a80e5cc619a1ddad1501e90fe7943edf3c8571
jianghm <jianghm@hzbenhe.com>
2014-05-30 18:05:17 +0800

    1

---
+++ src/main/java/com/jagamn/jutils/jutils/App.java
@@ -0,0 +1,13 @@
+package com.jagamn.jutils.jutils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
